8c51ce5053cac2cefccb1df4e59c5021027832e9
Jonathan Marcantonio <jmarcant@redhat.com>
2022-07-07 17:07:24 -0400

    reverting return err
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- pkg/controller/gitopscluster/gitopscluster_controller.go
+++ pkg/controller/gitopscluster/gitopscluster_controller.go
@@ -184,7 +184,7 @@ func (r *ReconcileGitOpsCluster) Reconcile(ctx context.Context, request reconcil
 	gitOpsClusters, err := r.GetAllGitOpsClusters()
 
 	if err != nil {
-		return reconcile.Result{Requeue: false}, err
+		return reconcile.Result{Requeue: false}, nil
 	}
 
 	// For any watched resource change, process all GitOpsCluster CRs to create new secrets or update existing secrets.
