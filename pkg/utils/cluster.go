c6cfce0dde12bdf5018a9f202fa7cdf8506d2587
Xiangjing Li <55890329+xiangjingli@users.noreply.github.com>
2024-12-18 22:30:57 -0500

    upgrade go to 1.22 (#108)
    Signed-off-by: Xiangjing Li <xiangli@redhat.com>

--- pkg/utils/cluster.go
+++ pkg/utils/cluster.go
@@ -46,10 +46,10 @@ const (
 )
 
 // ClusterPredicateFunc defines predicate function for cluster related watch, main purpose is to ignore heartbeat without change
-var ClusterPredicateFunc = predicate.Funcs{
-	UpdateFunc: func(e event.UpdateEvent) bool {
-		oldcl := e.ObjectOld.(*spokeClusterV1.ManagedCluster)
-		newcl := e.ObjectNew.(*spokeClusterV1.ManagedCluster)
+var ClusterPredicateFunc = predicate.TypedFuncs[*spokeClusterV1.ManagedCluster]{
+	UpdateFunc: func(e event.TypedUpdateEvent[*spokeClusterV1.ManagedCluster]) bool {
+		oldcl := e.ObjectOld
+		newcl := e.ObjectNew
 
 		//if managed cluster is being deleted
 		if !reflect.DeepEqual(oldcl.DeletionTimestamp, newcl.DeletionTimestamp) {
@@ -85,39 +85,27 @@ var ClusterPredicateFunc = predicate.Funcs{
 	},
 }
 
-var GitOpsClusterPredicateFunc = predicate.Funcs{
-	UpdateFunc: func(e event.UpdateEvent) bool {
-		oldGitOpsCluster := e.ObjectOld.(*gitopsclusterV1beta1.GitOpsCluster)
-		newGitOpsCluster := e.ObjectNew.(*gitopsclusterV1beta1.GitOpsCluster)
+var GitOpsClusterPredicateFunc = predicate.TypedFuncs[*gitopsclusterV1beta1.GitOpsCluster]{
+	UpdateFunc: func(e event.TypedUpdateEvent[*gitopsclusterV1beta1.GitOpsCluster]) bool {
+		oldGitOpsCluster := e.ObjectOld
+		newGitOpsCluster := e.ObjectNew
 
 		return !reflect.DeepEqual(oldGitOpsCluster.Spec, newGitOpsCluster.Spec)
 	},
 }
 
-var PlacementDecisionPredicateFunc = predicate.Funcs{
-	CreateFunc: func(e event.CreateEvent) bool {
-		decision, ok := e.Object.(*clusterv1beta1.PlacementDecision)
-
-		if !ok {
-			return false
-		}
-
-		klog.Infof("placement decision created, %v/%v", decision.Namespace, decision.Name)
+var PlacementDecisionPredicateFunc = predicate.TypedFuncs[*clusterv1beta1.PlacementDecision]{
+	CreateFunc: func(e event.TypedCreateEvent[*clusterv1beta1.PlacementDecision]) bool {
+		klog.Infof("placement decision created, %v/%v", e.Object.Namespace, e.Object.Name)
 		return true
 	},
-	DeleteFunc: func(e event.DeleteEvent) bool {
-		decision, ok := e.Object.(*clusterv1beta1.PlacementDecision)
-
-		if !ok {
-			return false
-		}
-
-		klog.Infof("placement decision deleted, %v/%v", decision.Namespace, decision.Name)
+	DeleteFunc: func(e event.TypedDeleteEvent[*clusterv1beta1.PlacementDecision]) bool {
+		klog.Infof("placement decision deleted, %v/%v", e.Object.Namespace, e.Object.Name)
 		return true
 	},
-	UpdateFunc: func(e event.UpdateEvent) bool {
-		oldDecision := e.ObjectOld.(*clusterv1beta1.PlacementDecision)
-		newDecision := e.ObjectNew.(*clusterv1beta1.PlacementDecision)
+	UpdateFunc: func(e event.TypedUpdateEvent[*clusterv1beta1.PlacementDecision]) bool {
+		oldDecision := e.ObjectOld
+		newDecision := e.ObjectNew
 
 		klog.Infof("placement decision updated, %v/%v", newDecision.Namespace, newDecision.Name)
 
@@ -125,119 +113,8 @@ var PlacementDecisionPredicateFunc = predicate.Funcs{
 	},
 }
 
-// AcmClusterSecretPredicateFunc defines predicate function for ACM cluster secrets watch
-var AcmClusterSecretPredicateFunc = predicate.Funcs{
-	UpdateFunc: func(e event.UpdateEvent) bool {
-		oldSecret, ok := e.ObjectOld.(*v1.Secret)
-		if !ok {
-			return false
-		}
-
-		newSecret, nok := e.ObjectNew.(*v1.Secret)
-		if !nok {
-			return false
-		}
-
-		oldSecretType, ok := e.ObjectOld.GetLabels()[ACMClusterSecretLabel]
-		newSecretType, nok := e.ObjectNew.GetLabels()[ACMClusterSecretLabel]
-
-		if ok && oldSecretType == "acm-cluster" {
-			klog.Infof("Update a old ACM cluster secret, old: %v/%v, new: %v/%v", oldSecret.Namespace, oldSecret.Name, newSecret.Namespace, newSecret.Name)
-			return true
-		}
-
-		if nok && newSecretType == "acm-cluster" {
-			klog.Infof("Update a new ACM cluster secret, old: %v/%v, new: %v/%v", oldSecret.Namespace, oldSecret.Name, newSecret.Namespace, newSecret.Name)
-			return true
-		}
-
-		klog.Infof("Not a ACM cluster secret update, old: %v/%v, new: %v/%v", oldSecret.Namespace, oldSecret.Name, newSecret.Namespace, newSecret.Name)
-		return false
-	},
-	CreateFunc: func(e event.CreateEvent) bool {
-		SecretType, ok := e.Object.GetLabels()[ACMClusterSecretLabel]
-
-		if !ok {
-			return false
-		} else if SecretType != "acm-cluster" {
-			return false
-		}
-
-		klog.Infof("Create a ACM cluster secret: %v/%v", e.Object.GetNamespace(), e.Object.GetName())
-		return true
-	},
-	DeleteFunc: func(e event.DeleteEvent) bool {
-		SecretType, ok := e.Object.GetLabels()[ACMClusterSecretLabel]
-
-		if !ok {
-			return false
-		} else if SecretType != "acm-cluster" {
-			return false
-		}
-
-		klog.Infof("Delete a ACM cluster secret: %v/%v", e.Object.GetNamespace(), e.Object.GetName())
-		return true
-	},
-}
-
-// ArgocdClusterSecretPredicateFunc defines predicate function for ArgoCD cluster secrets watch
-var ArgocdClusterSecretPredicateFunc = predicate.Funcs{
-	UpdateFunc: func(e event.UpdateEvent) bool {
-		oldSecret, ok := e.ObjectOld.(*v1.Secret)
-		if !ok {
-			return false
-		}
-
-		newSecret, nok := e.ObjectNew.(*v1.Secret)
-		if !nok {
-			return false
-		}
-
-		oldSecretType, ok := e.ObjectOld.GetLabels()[ArgocdClusterSecretLabel]
-		newSecretType, nok := e.ObjectNew.GetLabels()[ArgocdClusterSecretLabel]
-
-		if ok && oldSecretType == "true" {
-			klog.Infof("Update a old ArgoCD cluster secret, old: %v/%v, new: %v/%v", oldSecret.Namespace, oldSecret.Name, newSecret.Namespace, newSecret.Name)
-			return true
-		}
-
-		if nok && newSecretType == "true" {
-			klog.Infof("Update a new Argocd cluster secret, old: %v/%v, new: %v/%v", oldSecret.Namespace, oldSecret.Name, newSecret.Namespace, newSecret.Name)
-			return true
-		}
-
-		klog.Infof("Not a ArgoCD cluster secret update, old: %v/%v, new: %v/%v", oldSecret.Namespace, oldSecret.Name, newSecret.Namespace, newSecret.Name)
-		return false
-	},
-	CreateFunc: func(e event.CreateEvent) bool {
-		SecretType, ok := e.Object.GetLabels()[ArgocdClusterSecretLabel]
-
-		if !ok {
-			return false
-		} else if SecretType != "true" {
-			return false
-		}
-
-		klog.Infof("Create a ArgoCD cluster secret: %v/%v", e.Object.GetNamespace(), e.Object.GetName())
-		return true
-	},
-	DeleteFunc: func(e event.DeleteEvent) bool {
-		SecretType, ok := e.Object.GetLabels()[ArgocdClusterSecretLabel]
-
-		if !ok {
-			return false
-		} else if SecretType != "true" {
-			return false
-		}
-
-		klog.Infof("Delete a ArgoCD cluster secret: %v/%v", e.Object.GetNamespace(), e.Object.GetName())
-		return true
-	},
-}
-
-// ManagedClusterSecretPredicateFunc defines predicate function for managed cluster secrets watch
-var ManagedClusterSecretPredicateFunc = predicate.Funcs{
-	UpdateFunc: func(e event.UpdateEvent) bool {
+var ManagedClusterSecretPredicateFunc = predicate.TypedFuncs[*v1.Secret]{
+	UpdateFunc: func(e event.TypedUpdateEvent[*v1.Secret]) bool {
 		_, isSecretInArgo := e.ObjectNew.GetLabels()[ArgocdClusterSecretLabel]
 
 		if isSecretInArgo {
@@ -249,7 +126,7 @@ var ManagedClusterSecretPredicateFunc = predicate.Funcs{
 
 		return true
 	},
-	CreateFunc: func(e event.CreateEvent) bool {
+	CreateFunc: func(e event.TypedCreateEvent[*v1.Secret]) bool {
 		_, isSecretInArgo := e.Object.GetLabels()[ArgocdClusterSecretLabel]
 
 		if isSecretInArgo {
@@ -261,7 +138,7 @@ var ManagedClusterSecretPredicateFunc = predicate.Funcs{
 
 		return true
 	},
-	DeleteFunc: func(e event.DeleteEvent) bool {
+	DeleteFunc: func(e event.TypedDeleteEvent[*v1.Secret]) bool {
 		_, isSecretInArgo := e.Object.GetLabels()[ArgocdClusterSecretLabel]
 
 		if isSecretInArgo {
@@ -276,69 +153,10 @@ var ManagedClusterSecretPredicateFunc = predicate.Funcs{
 	},
 }
 
-// ArgocdServerPredicateFunc defines predicate function for cluster related watch
-var ArgocdServerPredicateFunc = predicate.Funcs{
-	UpdateFunc: func(e event.UpdateEvent) bool {
-		oldService, ok := e.ObjectOld.(*v1.Service)
-		if !ok {
-			return false
-		}
-
-		newService, nok := e.ObjectNew.(*v1.Service)
-		if !nok {
-			return false
-		}
-
-		oldArgocdServerLabel := e.ObjectOld.GetLabels()
-		newArgocdServerLabel := e.ObjectNew.GetLabels()
-
-		if oldArgocdServerLabel != nil && oldArgocdServerLabel["app.kubernetes.io/part-of"] == "argocd" &&
-			oldArgocdServerLabel["app.kubernetes.io/component"] == "server" {
-			klog.Infof("Update a old ArgoCD Server Service, old: %v/%v, new: %v/%v", oldService.Namespace, oldService.Name, newService.Namespace, newService.Name)
-			return true
-		}
-
-		if newArgocdServerLabel != nil && newArgocdServerLabel["app.kubernetes.io/part-of"] == "argocd" &&
-			newArgocdServerLabel["app.kubernetes.io/component"] == "server" {
-			klog.Infof("Update a new ArgoCD Server Service, old: %v/%v, new: %v/%v", oldService.Namespace, oldService.Name, newService.Namespace, newService.Name)
-			return true
-		}
-
-		klog.Infof("Not a ArgoCD Server service, old: %v/%v, new: %v/%v", oldService.Namespace, oldService.Name, newService.Namespace, newService.Name)
-		return false
-	},
-	CreateFunc: func(e event.CreateEvent) bool {
-		ArgocdServerLabel := e.Object.GetLabels()
-
-		if ArgocdServerLabel == nil {
-			return false
-		} else if ArgocdServerLabel["app.kubernetes.io/part-of"] != "argocd" ||
-			ArgocdServerLabel["app.kubernetes.io/component"] != "server" {
-			return false
-		}
-
-		klog.Infof("Create a ArgoCD Server Service: %v/%v", e.Object.GetNamespace(), e.Object.GetName())
-		return true
-	},
-	DeleteFunc: func(e event.DeleteEvent) bool {
-		ArgocdServerLabel := e.Object.GetLabels()
-
-		if ArgocdServerLabel == nil {
-			return false
-		} else if ArgocdServerLabel["app.kubernetes.io/part-of"] != "argocd" ||
-			ArgocdServerLabel["app.kubernetes.io/component"] != "server" {
-			return false
-		}
-
-		klog.Infof("Delete a ArgoCD Server Service: %v/%v", e.Object.GetNamespace(), e.Object.GetName())
-		return true
-	},
-}
-
-var ManagedServiceAccountPredicateFunc = predicate.Funcs{
-	UpdateFunc: func(e event.UpdateEvent) bool {
-		oldmsa := e.ObjectOld.(*authv1beta1.ManagedServiceAccount)
-		newmsa := e.ObjectNew.(*authv1beta1.ManagedServiceAccount)
+var ManagedServiceAccountPredicateFunc = predicate.TypedFuncs[*authv1beta1.ManagedServiceAccount]{
+	UpdateFunc: func(e event.TypedUpdateEvent[*authv1beta1.ManagedServiceAccount]) bool {
+		oldmsa := e.ObjectOld
+		newmsa := e.ObjectNew
 
 		secretUpdated := !reflect.DeepEqual(oldmsa.Status.TokenSecretRef, newmsa.Status.TokenSecretRef)
 
@@ -346,8 +164,8 @@ var ManagedServiceAccountPredicateFunc = predicate.Funcs{
 
 		return secretUpdated
 	},
-	CreateFunc: func(e event.CreateEvent) bool {
-		msa := e.Object.(*authv1beta1.ManagedServiceAccount)
+	CreateFunc: func(e event.TypedCreateEvent[*authv1beta1.ManagedServiceAccount]) bool {
+		msa := e.Object
 
 		if msa.Status.TokenSecretRef != nil {
 			return true
@@ -357,8 +175,8 @@ var ManagedServiceAccountPredicateFunc = predicate.Funcs{
 
 		return false
 	},
-	DeleteFunc: func(e event.DeleteEvent) bool {
-		msa := e.Object.(*authv1beta1.ManagedServiceAccount)
+	DeleteFunc: func(e event.TypedDeleteEvent[*authv1beta1.ManagedServiceAccount]) bool {
+		msa := e.Object
 
 		if msa.Status.TokenSecretRef != nil {
 			return true
